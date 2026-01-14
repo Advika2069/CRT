aa295273c29b7c233279af33ee60af73d5a868c8
Advika2069 <advikakanchanapally@gmail.com>
2026-01-14 15:29:13 +0530

    Update task.py

--- IV th-sem/Python/assignments/M01_Language_and_Programming_Basics/AST03/task.py
+++ IV th-sem/Python/assignments/M01_Language_and_Programming_Basics/AST03/task.py
@@ -1,1 +1,11 @@
-
+def Student_Grade_System(name,n1,n2,n3):
+    average = (n1+n2+n3 )/ 3
+    if average >= 40:
+        status = "Pass"
+    else:
+        status = "Fail"
+    print(f"Average grade: {average: .2f},Status: {status}")
+if __name__ == "__main__":
+    name = input()
+    n1,n2,n3 = map(int,input().split())
+    Student_Grade_System(name,n1,n2,n3)

3263f0e5304fca6712c7561f310217cbee0cfeca
Advika2069 <advikakanchanapally@gmail.com>
2026-02-02 19:32:20 +0530

    Update PS03_operators.py

--- IV th-sem/Python/Practice/M01_Language_and_Programming_Basics/S02/PS03_operators.py
+++ IV th-sem/Python/Practice/M01_Language_and_Programming_Basics/S02/PS03_operators.py
@@ -1,1 +1,19 @@
 
+x = int(input("Enter first number: "))
+print(x<<2)
+print(x>>2)
+print(~x)
+print()
+# membership operator: in,not in 
+s = input() 
+print("yth" in s)
+print()
+# identity operator: is , is not
+x = 10
+y = 20
+print(x is y)
+print(x is not y)
+print()
+l1 = [1,2,3]
+l2 = [1,2,3]
+print(l1 is l2)
